bee18c2d7d26c25f7158da1ef0e61e6fb0778bf4
Ahmet Can BOLAT <acanbolat@outlook.com>
2023-10-24 16:58:03 +0300

    END OF SECTION 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Products } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CNNET';
+  products: Products[] = [];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get<Pagination<Products[]>>('http://localhost:5001/api/product?pageSize=50').subscribe({
+      next: response => this.products = response.data, // what to do next
+      error: (error) => console.log(error), //what to do if there is an error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+      },
+    });
+  }
 }
